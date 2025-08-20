4cbf23b5ca1e6ae8187c609eabcaed9aa0471364
deepa-m1 <dhulasidharandeepamalika@gmail.com>
2025-08-21 00:40:58 +0800

    Renamed main class and added greeting

---
+++ src/main/java/Cupcake.java
@@ -0,0 +1,6 @@
+public class Cupcake {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Cupcake\n" + "What can I do for you?");
+        System.out.println("Bye.Hope to see you again soon!");
+    }
+}
