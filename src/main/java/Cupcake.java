3df74f961763dd22c0aee7b6c325a8a11fafc643
deepa-m1 <dhulasidharandeepamalika@gmail.com>
2025-08-21 17:37:15 +0800

    added the add & list features that tracks userinput

--- src/main/java/Cupcake.java
+++ src/main/java/Cupcake.java
@@ -1,5 +1,6 @@
 //to get user input to do echo
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Cupcake {
     public static void main(String[] args) {
@@ -9,9 +10,22 @@ public class Cupcake {
 
         //storing the actual input
         String userInput = askInput.nextLine();
+        ArrayList<String> inputList = new ArrayList<>();
+
         //while the userInput is not Bye we just print as it is
         while(!(userInput.equals("bye") || userInput.equals("Bye") || userInput.equals("BYE"))) {
-            System.out.println(userInput);
+            //if input was list then printing instructions
+            if(userInput.equals("list")) {
+                for(int i = 0; i < inputList.size();i++) {
+                    int p = 1 + i;
+                    System.out.println(p + ". " + inputList.get(i));
+                }
+            } else {
+                //if not list then add the user input into the list
+                inputList.add(userInput);
+                System.out.println("added: " + userInput);
+            }
+
             //prompt for another input
             System.out.println("*****************\n" + "Anything else I may help you with?");
             askInput = new Scanner(System.in);
@@ -19,5 +33,6 @@ public class Cupcake {
         }
 
         System.out.println("Bye. Hope to see you again real soon!");
+
     }
 }
