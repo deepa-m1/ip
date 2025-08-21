d71a9a30f8ba590286fd9c296f08c2dfd5e4c908
deepa-m1 <dhulasidharandeepamalika@gmail.com>
2025-08-21 19:48:40 +0800

    added Task class-chabot can do marking/unmarking of tasks

--- src/main/java/Cupcake.java
+++ src/main/java/Cupcake.java
@@ -8,28 +8,50 @@ public class Cupcake {
         Scanner askInput = new Scanner(System.in);
         System.out.println("Hello! I'm Cupcake :)\n" + "What can I do for you?");
 
-        //storing the actual input
-        String userInput = askInput.nextLine();
-        ArrayList<String> inputList = new ArrayList<>();
+        //storing the actual input into a task array list
+        String txtInput = askInput.nextLine();
+        ArrayList<Task> taskList = new ArrayList<>();
 
         //while the userInput is not Bye we just print as it is
-        while(!(userInput.equals("bye") || userInput.equals("Bye") || userInput.equals("BYE"))) {
+        while(!(txtInput.equals("bye") || txtInput.equals("Bye") || txtInput.equals("BYE"))) {
+            //taskInput will reset each time(after new user input)
+            Task taskInput = new Task(txtInput);
+
             //if input was list then printing instructions
-            if(userInput.equals("list")) {
-                for(int i = 0; i < inputList.size();i++) {
+            if(txtInput.equals("list")) {
+                for(int i = 0; i < taskList.size(); i++) {
                     int p = 1 + i;
-                    System.out.println(p + ". " + inputList.get(i));
+                    Task currtask = taskList.get(i);
+                    System.out.println(p + ".[" + currtask.getStatus() + "] " +currtask.getDescription());
                 }
-            } else {
-                //if not list then add the user input into the list
-                inputList.add(userInput);
-                System.out.println("added: " + userInput);
             }
 
-            //prompt for another input
+            //using Task functions if mark/unmark
+            else if(txtInput.startsWith("mark")) {
+                int num = (txtInput.charAt(5)) - '0';
+                Task markTask = taskList.get(num-1);
+                markTask.markAsDone();
+                System.out.println("Wow look at you go champion!! I will mark it.\n" +
+                      "[" + markTask.getStatus() + "] " + markTask.getDescription());
+            }
+            else if(txtInput.startsWith("unmark")) {
+                int num = (txtInput.charAt(7)) - '0';
+                Task unmarkTask = taskList.get(num-1);
+                unmarkTask.unMark();
+                System.out.println("Okay noted!! I will unmark it.\n" +
+                      "[" + unmarkTask.getStatus() + "] " + unmarkTask.getDescription());
+            }
+
+            else {
+                //if not list then add the user input into the list
+                taskList.add(taskInput);
+                System.out.println("added: " + txtInput);
+            }
+
+            //prompt for any other input
             System.out.println("*****************\n" + "Anything else I may help you with?");
             askInput = new Scanner(System.in);
-            userInput = askInput.nextLine();
+            txtInput = askInput.nextLine();
         }
 
         System.out.println("Bye. Hope to see you again real soon!");
---
+++ src/main/java/Task.java
@@ -0,0 +1,30 @@
+public class Task {
+    //the characteristics of a Task -fields
+    protected String description;
+    protected boolean isDone;
+
+    //constructor
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    //getters since protected fields
+    public String getStatus() {
+        return (isDone ? "X" : " ");
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    //marking done
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    //undoing accidental marking
+    public void unMark() {
+        this.isDone = false;
+    }
+}
