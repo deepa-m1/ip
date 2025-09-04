4ded04201ecc42dcc56c7371a5529c1f716d6fe2
deepa-m1 <dhulasidharandeepamalika@gmail.com>
2025-09-05 02:32:57 +0800

    Add cupcake.gui & resources
    CLI: user runs program from Cupcake.java
    
    Does not have user-friendly attractive GUI.
    
    Let's use JavaFx to design a minimalistic GUI.
    
    JavaFx is standard way to design GUI for java code.

--- src/main/java/cupcake/ui/Cupcake.java
+++ src/main/java/cupcake/ui/Cupcake.java
@@ -2,6 +2,10 @@ package cupcake.ui;//to get user input to do echo
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+
 public class Cupcake {
     //fields
     /** Hard-disk file is stored in Storage */
@@ -11,6 +15,17 @@ public class Cupcake {
     /** the user interface*/
     private Ui ui;
 
+    /** the GUI userInput*/
+    private String userInput;
+
+    /** the default System.out statement*/
+    private PrintStream defaultStream = System.out;
+    /** the Ui print statements array*/
+    private ByteArrayOutputStream uiOutputArray = new ByteArrayOutputStream();
+    /** the Ui print statements stream*/
+    private PrintStream uiOutputStream = new PrintStream(uiOutputArray);
+
+
     /**
      * Creates new Cupcake object.
      * Creates new Ui and Storage objects within.
@@ -19,18 +34,50 @@ public class Cupcake {
      * @param filePath path to the stored file in Hard-disk.
      */
     public Cupcake(String filePath) {
+        //So that our output stream is reassigned
+        System.setOut(uiOutputStream);
+
         ui = new Ui();
         storage = new Storage(filePath);
         ui.intro();
 
+
         try {
             tasks = new TaskList(storage.getFileContent(filePath));
             ui.printWelcomeBack();
+            this.getResponse();
         } catch (FileNotFoundException e) {
             ui.printFileRetrieveError();
             tasks = new TaskList();
+            this.getResponse();
         }
     }
+
+    /**
+     * Returns string version of print outputs.
+     * @return String of print statements at that point in time.
+     */
+    public String getResponse() {
+        //writes buffered output to the stream
+        System.out.flush();
+        String output = uiOutputArray.toString();
+        return output;
+    }
+
+
+    //setter for userInput
+    public void inputSetter(String input) {
+        this.userInput = input;
+    }
+
+    /**
+     * Resets the uiOutputArray so that it is empty for
+     * subsequent user input event interaction.
+     */
+    public void resetOutputArray() {
+        uiOutputArray.reset();
+    }
+
 
     /**
      * Writes user's tasks into Hard-disk file once program exits.
@@ -41,12 +88,10 @@ public class Cupcake {
         //does the interface situation
 
         //storing the actual input into a task array list
-        String txtInput = ui.getInput();
-
         //while the userInput is not Bye we just print as it is
-        while (!(txtInput.equals("bye") || txtInput.equals("Bye") || txtInput.equals("BYE"))) {
+        if (!(userInput.equals("bye") || userInput.equals("Bye") || userInput.equals("BYE"))) {
             //duke.ui.Parser object
-            Parser parseObj = new Parser(txtInput);
+            Parser parseObj = new Parser(userInput);
             String keyWord = parseObj.getKeyWord();
             int num;
 
@@ -101,22 +146,27 @@ public class Cupcake {
                         tasks.add(taskInput);
                         ui.printSuccessfullyAdded(taskInput, tasks.size());
                     }
+
+            this.getResponse();
             }
 
             //prompt for nxt new input
             ui.formattedAsk();
-            txtInput = ui.getInput();
+            this.getResponse();
 
         }
 
-        //since user input is Bye, write to storedFile new inputs
-        try {
-            String content = tasks.currTaskListStr();
-            storage.writeToFile(content);
-        } catch (IOException e) {
-            ui.printCannotSaveFile();
+        else {
+
+            //since user input is Bye, write to storedFile new inputs
+            try {
+                String content = tasks.currTaskListStr();
+                storage.writeToFile(content);
+            } catch (IOException e) {
+                ui.printCannotSaveFile();
+            }
+            ui.printBye();
         }
-        ui.printBye();
 
     }
 
